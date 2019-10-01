4769603ea02ec31f1b5816db61efb668250d0d5d
Eshan <eshan1285@gmail.com>
2019-10-01 16:59:50 +0530

    adding the files

---
+++ build/precache-manifest.52ac94b39c644b0244aca88a783d41bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc313bbc4badc9e14dd931ed16eb2495",
+    "url": "/eshanheretic/Robofriends.git/index.html"
+  },
+  {
+    "revision": "279bf51e7864db1c746b",
+    "url": "/eshanheretic/Robofriends.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "67ddf8a115cab00e517a",
+    "url": "/eshanheretic/Robofriends.git/static/css/main.95a62e81.chunk.css"
+  },
+  {
+    "revision": "279bf51e7864db1c746b",
+    "url": "/eshanheretic/Robofriends.git/static/js/2.761caa3a.chunk.js"
+  },
+  {
+    "revision": "67ddf8a115cab00e517a",
+    "url": "/eshanheretic/Robofriends.git/static/js/main.01dca3bd.chunk.js"
+  },
+  {
+    "revision": "80244f7cdbaa355365d9",
+    "url": "/eshanheretic/Robofriends.git/static/js/runtime-main.c9383359.js"
+  }
+]);
