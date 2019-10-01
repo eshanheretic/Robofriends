80c27a00b32fc15ce81e3c69403ff78b0169f54a
Eshan <eshan1285@gmail.com>
2019-10-01 17:05:12 +0530

    Updates

---
+++ precache-manifest.0a22533fcb32ee8306352a9a985780dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7f3e02d3f66a17e3d26a6bece21526",
+    "url": "/eshanheretic/Robofriends/index.html"
+  },
+  {
+    "revision": "279bf51e7864db1c746b",
+    "url": "/eshanheretic/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1a18ca6b3af98aefa1d5",
+    "url": "/eshanheretic/Robofriends/static/css/main.95a62e81.chunk.css"
+  },
+  {
+    "revision": "279bf51e7864db1c746b",
+    "url": "/eshanheretic/Robofriends/static/js/2.761caa3a.chunk.js"
+  },
+  {
+    "revision": "1a18ca6b3af98aefa1d5",
+    "url": "/eshanheretic/Robofriends/static/js/main.836d0820.chunk.js"
+  },
+  {
+    "revision": "3456a1ea4313428efc31",
+    "url": "/eshanheretic/Robofriends/static/js/runtime-main.8064f8d8.js"
+  }
+]);
